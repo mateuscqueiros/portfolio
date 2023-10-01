2c5ed0c55de99f49cce2abfc1dbc73796dd9972c
Mateus Queirós <mateuscqueiros1@gmail.com>
2023-10-01 02:48:59 -0300

    readme

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,10 @@
+import { ExternalLink } from "../ExternalLink";
+
 export function Footer() {
   return (
     <footer className="flex justify-center w-full ">
-      Feito com &#60;3, Next.js e TailwindCSS
+      Feito com &#60;3, Next.js e TailwindCSS. Deploy feito na Vercel. Ver{" "}
+      <ExternalLink href="">códig-fonte</ExternalLink> .
     </footer>
   );
 }
